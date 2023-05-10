9c24808857e0c901848b7b5760d40618e4941fcb
Natanael Pacheco <natepacheco716@gmail.com>
2023-05-10 04:38:59 -0500

    implemented providers callback function

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,16 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async (providers) => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -43,9 +53,23 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
+
+      {/* Mobile Navigation - LEFT OFF HERE */}
     </nav>
   );
 };
